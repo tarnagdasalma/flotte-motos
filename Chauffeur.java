d97c0ad6c6ea4b8f52424b07c2764389d50011c3
josephineouedraogo968-droid <josephineouedraogo968@gmail.com>
2026-06-28 14:31:18 +0200

    Ajout de la classe Chauffeur

---
+++ Chauffeur.java
@@ -0,0 +1,16 @@
+public class Chauffeur {
+    private String nom;
+    private String numeroPermis;
+
+    public Chauffeur(String nom, String numeroPermis) {
+        this.nom = nom;
+        this.numeroPermis = numeroPermis;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getNumeroPermis() {
+        return numeroPermis;
+    }
